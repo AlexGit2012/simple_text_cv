5c3b2968793952aead0e26c4c46b4b123d51d692
AlexGit2012 <a.karavaychik@gmail.com>
2020-09-11 12:54:03 +0300

    simple_text_cv_build

---
+++ precache-manifest.ca641f43c99d369de1eeb011b302a39d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6f33e57acec2d89aed100e1d46ef13",
+    "url": "/AlexGit2012/simple_text_cv/index.html"
+  },
+  {
+    "revision": "9c5db80be6f8a5673689",
+    "url": "/AlexGit2012/simple_text_cv/static/css/main.58a74033.chunk.css"
+  },
+  {
+    "revision": "32429365a66933cb0137",
+    "url": "/AlexGit2012/simple_text_cv/static/js/2.90a71843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlexGit2012/simple_text_cv/static/js/2.90a71843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c5db80be6f8a5673689",
+    "url": "/AlexGit2012/simple_text_cv/static/js/main.55b84718.chunk.js"
+  },
+  {
+    "revision": "368f7cf86c605495159b",
+    "url": "/AlexGit2012/simple_text_cv/static/js/runtime-main.a5c62ad2.js"
+  },
+  {
+    "revision": "748e26febb0e59207f7b9781b53b15f8",
+    "url": "/AlexGit2012/simple_text_cv/static/media/dot.748e26fe.svg"
+  },
+  {
+    "revision": "de2a9158253dfcb6396e997317ae6c40",
+    "url": "/AlexGit2012/simple_text_cv/static/media/photo.de2a9158.png"
+  }
+]);
